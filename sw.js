fd0f0ee427b3a317d05c22214878a2507a5188db
ユッフィー1111 <yuffyphoenix@gmail.com>
2026-06-23 21:00:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 //   完全バイパス（キャッシュしない・読まない）:
 //     - testtool*.js, api.github.com         ← 認証必須のため常時オンライン取得
 
-const CACHE_VERSION = '1.0.6β+';
+const CACHE_VERSION = '1.0.7β+';
 const CACHE_NAME = `dqx-tools-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
